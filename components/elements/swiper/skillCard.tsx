977c1584f711645fe3450d2bef9780f9ee7c5dcb
Josumaru <jokosupriyanto1712@gmail.com>
2023-09-11 21:52:14 +0700

    remove unused codeline

--- components/elements/swiper/skillCard.tsx
+++ components/elements/swiper/skillCard.tsx
@@ -6,7 +6,6 @@ import { PiCodesandboxLogoThin } from "react-icons/pi"
 const SkillCard = () => {
     return (
         <div className={`${styles.skills_subsection}  p-5 my-4 rounded-md h-5/6 flex items-center flex-col`} data-aos="fade-up-right">
-            {/* <Image className={`w-1/3 py-4`} src={reactIcon} alt={"React JS"}></Image> */}
             <div className={`text-emerald-500 text-9xl`}>
                 <PiCodesandboxLogoThin />
             </div>
