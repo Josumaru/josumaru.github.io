21ae88afce90b61299a92dd88fbb3675d395a66a
Josumaru <jokosupriyanto1712@gmail.com>
2024-10-22 21:08:41 +0700

    fix: fix tailwind duplicate config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ const config = {
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
+  		center: true,
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -110,30 +110,12 @@ const config = {
   					transform: 'translate(-50%,-40%) scale(1)'
   				}
   			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
   			'infinite-scroll': 'scroll 30s linear infinite',
   			spotlight: 'spotlight 2s ease .75s 1 forwards',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
